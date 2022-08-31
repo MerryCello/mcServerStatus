28b7afabd612fa7f9e2a867c64d4033c5b7c58b5
GrumpyCello <kfoniciello@gmail.com>
2022-08-31 17:45:43 -0600

    converted project to React

---
+++ src/components/Button.js
@@ -0,0 +1,17 @@
+import React from "react";
+import buttonPlate from "../images/Button_Plate.mp3";
+
+const Button = React.forwardRef(({children, onClick, style, disabled}, ref) => {
+   const buttonPlateAudio = new Audio(buttonPlate);
+
+   const onClickCb = () => {
+      buttonPlateAudio.play();
+      onClick();
+   } 
+
+   return (
+      <button ref={ref} disabled={disabled} className="button" onClick={onClickCb} style={style}>{children}</button>
+   );
+});
+
+export default Button;
---
+++ src/components/Signal.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react"
+import bar1 from "../images/1-5_Signal.png"
+import bar2 from "../images/2-5_Signal.png"
+import bar3 from "../images/3-5_Signal.png"
+import bar4 from "../images/4-5_Signal.png"
+import fullSignal from "../images/fullSignal.png"
+import noSignal from "../images/noSignal.png"
+import searchingSignal from "../images/searchingSignal.png"
+
+
+const Signal = ({serverStatus, size, style}) => {
+   const [sigImg, setSigImg] = useState(searchingSignal)
+
+   useEffect(() => {
+      if (serverStatus?.online)
+        setSigImg(fullSignal)
+      else if (serverStatus?.loading)
+         setSigImg(searchingSignal)
+      else
+         setSigImg(noSignal)
+   }, [serverStatus])
+
+   return (
+      <div style={style}>
+         <img src={sigImg} style={{height: size + "em", width: "auto"}}/>
+      </div>
+   );
+}
+
+export default Signal;
