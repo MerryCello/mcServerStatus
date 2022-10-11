e395b3e196a474999bdf5f6295bf81f04ce3d228
GrumpyCello <kfoniciello@gmail.com>
2022-10-10 21:21:27 -0600

    made audio file faster

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,27 @@
 import React from "react";
 import buttonPlate from "../images/Button_Plate.mp3";
 
-const Button = React.forwardRef(({children, onClick, style, disabled}, ref) => {
-   const buttonPlateAudio = new Audio(buttonPlate);
+const Button = React.forwardRef(
+  ({ children, onClick, style, disabled }, ref) => {
+    const buttonPlateAudio = new Audio(buttonPlate);
 
-   const onClickCb = () => {
+    const onClickCb = () => {
       buttonPlateAudio.play();
       onClick();
-   } 
+    };
 
-   return (
-      <button ref={ref} disabled={disabled} className="button" onClick={onClickCb} style={style}>{children}</button>
-   );
-});
+    return (
+      <button
+        ref={ref}
+        disabled={disabled}
+        className="button"
+        onClick={onClickCb}
+        style={style}
+      >
+        {children}
+      </button>
+    );
+  }
+);
 
 export default Button;
