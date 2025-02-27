b13260643de874c0802c190bff4b91df41d0dcbe
GrumpyCello <kfoniciello@gmail.com>
2025-02-26 21:39:40 -0600

    refactor: prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Navigate,
-} from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import NotFound404 from "./pages/NotFound404";
-import EditServerPage from "./pages/EditServerPage";
-import DeleteServerPage from "./pages/DeleteServerPage";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {createBrowserRouter, RouterProvider, Navigate} from 'react-router-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import NotFound404 from './pages/NotFound404';
+import EditServerPage from './pages/EditServerPage';
+import DeleteServerPage from './pages/DeleteServerPage';
 
 const router = createBrowserRouter([
   {
-    path: "*",
+    path: '*',
     element: <Navigate to='/mcServerStatus' replace />,
     errorElement: <NotFound404 />,
   },
@@ -24,41 +20,41 @@ const router = createBrowserRouter([
     errorElement: <NotFound404 />,
   },
   {
-    path: "/mcServerStatus/edit",
+    path: '/mcServerStatus/edit',
     element: <EditServerPage />,
     errorElement: <NotFound404 />,
   },
   {
-    path: "/edit",
+    path: '/edit',
     element: <Navigate to='/mcServerStatus/edit' replace />,
     errorElement: <NotFound404 />,
   },
   {
-    path: "/mcServerStatus/add",
+    path: '/mcServerStatus/add',
     element: <EditServerPage />,
     errorElement: <NotFound404 />,
   },
   {
-    path: "/add",
+    path: '/add',
     element: <Navigate to='/mcServerStatus/add' replace />,
     errorElement: <NotFound404 />,
   },
   {
-    path: "/mcServerStatus/delete",
+    path: '/mcServerStatus/delete',
     element: <DeleteServerPage />,
     errorElement: <NotFound404 />,
   },
   {
-    path: "/delete",
+    path: '/delete',
     element: <Navigate to='/mcServerStatus/delete' replace />,
     errorElement: <NotFound404 />,
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
